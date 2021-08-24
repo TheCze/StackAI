7e4df9a89aee913b9987a272747c99af0ce80f69
Daniel Czeppel <danielczeppel@gmail.com>
2021-08-24 18:33:41 +0200

    Added hash function to Pos2D

--- Pos2D.h
+++ Pos2D.h
@@ -3,15 +3,19 @@
 
 struct Pos2D {
 	Pos2D(int a, int b): x(a), y(b) {}
-	//Pos2D(const Pos2D& cpy) : x(cpy.x), y(cpy.y) {}
 	Pos2D(){}
 	int x = 0;
 	int y = 0;
+	int saved_hash = -1;
 	inline bool operator==(const Pos2D& b) { return x == b.x && y == b.y; }
 	Pos2D operator+(const Pos2D& b) { return Pos2D(x + b.x, y + b.y); }
 	inline bool operator!=(const Pos2D& b) { return x != b.x || y != b.y; }
 	Pos2D Adjacent(int px, int py) { return Pos2D(x + px, y + py); }
-
+	int hash() {
+		if(saved_hash == -1)
+			saved_hash = ((x + y) * (x + y + 1) / 2 + y);
+		return saved_hash;
+	}
 	void MoveTowards(Pos2D target){
 		if (x > target.x)
 			x--;
