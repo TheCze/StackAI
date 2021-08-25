0b9bb149d4e0547aac828c438401cc6fba543b43
Daniel Czeppel <danielczeppel@gmail.com>
2021-08-25 11:19:11 +0200

    Clean up

--- OpenList.cpp
+++ OpenList.cpp
@@ -12,19 +12,3 @@ AStarNode OpenList::PopLowestCost()
 	heap.pop();
 	return min;
 }
-
-void OpenList::DebugPrint()
-{
-	std::cout << "==Open List==" << std::endl;
-	for (AStarNode& node : list) {
-		if (node.previous)
-		std::cout << node.tile.pos_.x << " / " << node.tile.pos_.y << " (From: "
-			<< node.previous->tile.pos_.x << " / " << node.previous->tile.pos_.y << ")" << std::endl
-			<<"Tile: " << node.pathcost <<" Heuristic: "<< node.estcost- node.pathcost << " Total: " << node.estcost << std::endl;
-		else {
-			std::cout << node.tile.pos_.x << " / " << node.tile.pos_.y << " (START)" << std::endl
-				<< "Tile: " << node.pathcost << " Heuristic: " << node.estcost - node.pathcost << " Total: " << node.estcost << std::endl;
-		}
-	}
-	std::cout << "===============" << std::endl;
-}
--- OpenList.h
+++ OpenList.h
@@ -6,10 +6,8 @@
 struct OpenList {
 	void AddOrUpdate(AStarNode node);
 	AStarNode PopLowestCost();
-	void DebugPrint();
 	bool empty() { return heap.empty(); };
 	int size() { return heap.size(); }
 private:
-	std::vector<AStarNode> list;
 	std::priority_queue<AStarNode> heap;
 };
